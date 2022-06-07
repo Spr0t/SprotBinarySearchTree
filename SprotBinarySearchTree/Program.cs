06fcd507a8cccde4fcf7b88e8fa634dd787f23ec
Serj Prots <serganas995@gmail.com>
2022-06-07 18:30:19 +0300

    Добавьте файлы проекта.

---
+++ SprotBinarySearchTree/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprotBinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new Tree<int>();
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(7);
+            tree.Add(1);
+            tree.Add(2);
+            tree.Add(8);
+            tree.Add(6);
+            tree.Add(9);
+
+            foreach (var item in tree.Preorder())
+            {
+                Console.Write(item + ", ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
